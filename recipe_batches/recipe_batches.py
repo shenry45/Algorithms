676ba037db5bdf6aed41adcd8798caa09585796e
shenry45 <shawn45henry@gmail.com>
2020-02-04 19:41:51 -0700

    recipe_batches through loops

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,32 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # track number of possible batches
+  ingredient_counts = []
+
+  # check if all ingredients are available
+  for ingredient in recipe:
+    # if not available
+    if ingredient in ingredients:
+      pass
+    else:
+      return 0
+
+  # run for all available ingredients
+  for ingredient in ingredients:
+    # if ingredient is not found pass
+    if recipe[ingredient] == False:
+      pass
+    # else how many of current is divisible by recipe
+    else:
+      if (ingredients[ingredient] / recipe[ingredient]) < 1:
+        return 0
+      # else return current ingredient // recipe ingredient
+      else:
+        ingredient_counts.append(ingredients[ingredient] // recipe[ingredient])
+
+  # find min of batch list
+  return min(ingredient_counts)
 
 
 if __name__ == '__main__':
